bdfe1085add593eed884727df6d654fb2b1c2b6a
Muneer Shaik <mshaik4@berkadia.com>
2025-09-07 11:17:25 +0530

    practice

--- eksctl/bastion.tf
+++ eksctl/bastion.tf
@@ -2,6 +2,7 @@ module "bastion" {
   source = "terraform-aws-modules/ec2-instance/aws"
   name = "bastion"
   instance_type = "t3.micro"
+  associate_public_ip_address = true
   vpc_security_group_ids = ["sg-0e1f86e2a2f5e55fb"]
   subnet_id = "subnet-09068423ffac2f567"
   ami = data.aws_ami.ami_info.id
