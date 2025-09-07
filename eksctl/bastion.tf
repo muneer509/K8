ed2979a183b2711797261f9df095fc936e849ac7
Muneer Shaik <mshaik4@berkadia.com>
2025-09-07 10:52:27 +0530

    practice

---
+++ eksctl/bastion.tf
@@ -0,0 +1,13 @@
+module "bastion" {
+  source = "terraform-aws-modules/ec2-instance/aws"
+  name = "bastion"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0e1f86e2a2f5e55fb"]
+  subnet_id = "subnet-09068423ffac2f567"
+  ami = data.aws_ami.ami_info.id
+  user_data = file("workstation.sh")
+  tags = {
+    Name = "bastion"
+  }
+
+}
